1a6690cf79f3595cd09ba490ead5b120fcfbbffe
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-05-06 20:27:06 +0300

    files updated

--- src/App.js
+++ src/App.js
@@ -2,18 +2,64 @@ import React, { Component } from 'react';
 import './styles/base.scss';
 import TodoList from './components/TodoList/TodoList';
 import initialTodos from '../src/todos.json';
+import TodoEditor from './components/TodoEditor/TodoEditor';
+import { nanoid } from 'nanoid';
 
 class App extends Component {
   state = {
     todos: initialTodos,
   };
 
+  deleteTodo = todoId => {
+    this.setState(prevState => ({ todos: prevState.todos.filter(todo => todo.id !== todoId) }));
+  };
+
+  toggleCompleted = todoId => {
+    this.setState(prevState => ({
+      todos: prevState.todos.map(todo => {
+        if (todo.id === todoId) {
+          console.log('Знайшли потрібний todo!');
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      }),
+    }));
+  };
+
+  addTodo = text => {
+    const todo = {
+      id: nanoid(),
+      text: text,
+      completed: false,
+    };
+    this.setState(prevState => ({ todos: [...prevState.todos, todo] }));
+  };
+
   render() {
     const { todos } = this.state;
+
+    const totalTodos = todos.length;
+    const completedTodosCount = todos.reduce(
+      (total, todo) => (todo.completed ? total + 1 : total),
+      0
+    );
+
     return (
       <div>
         <h1>Стан компонента</h1>
-        <TodoList todos={todos} />
+        <div>
+          <p>Загальна кількість todo'шек: {totalTodos}</p>
+          <p>Кількість виконаних todo'шек: {completedTodosCount}</p>
+        </div>
+        <TodoEditor onSubmit={this.addTodo} />
+        <TodoList
+          todos={todos}
+          onDeleteTodo={this.deleteTodo}
+          onToggleCompleted={this.toggleCompleted}
+        />
       </div>
     );
   }
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,9 +1,22 @@
-const TodoList = ({ todos }) => (
+import React from 'react';
+import './TodoList.scss';
+import classNames from 'classnames';
+
+const TodoList = ({ todos, onDeleteTodo, onToggleCompleted }) => (
   <ul className="TodoList">
-    {todos.map(({ id, text }) => (
-      <li key={id} className="TodoList__item">
+    {todos.map(({ id, text, completed }) => (
+      <li
+        key={id}
+        className={classNames('TodoList__item', { 'TodoList__item--completed': completed })}
+      >
+        <input
+          type="checkbox"
+          className="TodoList__checkbox"
+          checked={completed}
+          onChange={() => onToggleCompleted(id)}
+        ></input>
         <p className="TodoList__text">{text}</p>
-        <button>Удалити</button>
+        <button onClick={() => onDeleteTodo(id)}>Удалити</button>
       </li>
     ))}
   </ul>
