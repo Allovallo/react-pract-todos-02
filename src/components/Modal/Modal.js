b03f3b917c4239c8f784a19c43ea4ef15014e2a9
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-05-08 17:39:59 +0300

    Modal updated

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -6,24 +6,38 @@ const modalRoot = document.querySelector('#modal-root');
 
 export default class Modal extends Component {
   componentDidMount() {
-    console.log('Modal componentDidMount');
+    // console.log('Modal componentDidMount');
 
-    window.addEventListener('keydown', event => {
-      if (event.code === 'Escape') {
-        console.log('Ви клікнули Escape!');
-
-        this.props.onClose();
-      }
-    });
+    window.addEventListener('keydown', this.handleKeyDown);
   }
 
   componentWillUnmount() {
-    // console.log('Modal componentWillUnmount');
+    console.log('Modal componentWillUnmount');
+
+    window.removeEventListener('keydown', this.handleKeyDown);
   }
+
+  handleKeyDown = event => {
+    if (event.code === 'Escape') {
+      console.log('Ви клікнули Escape!');
+
+      this.props.onClose();
+    }
+  };
+
+  handleBackdropClick = event => {
+    // console.log('Клікнули на сірому!');
+    // console.log('currentTarget: ', event.currentTarget);
+    // console.log('target: ', event.target);
+
+    if (event.currentTarget === event.target) {
+      this.props.onClose();
+    }
+  };
 
   render() {
     return createPortal(
-      <div className="Modal__backdrop">
+      <div className="Modal__backdrop" onClick={this.handleBackdropClick}>
         <div className="Modal__content">{this.props.children}</div>
       </div>,
       modalRoot
